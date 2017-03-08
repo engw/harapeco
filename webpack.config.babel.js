7cd7344fc0367700039219950cec3c3b6ea0d711
chck <candysynd@gmail.com>
2017-03-09 02:34:28 +0900

    [fix] webpack.config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,26 +1,26 @@
 export default {
   entry: {
-    main: 'src/main.js',
-    output: {
-      path: __dirname,
-      filename: '[name].js'
-    },
-    module: {
-      loaders: [{
-        test: /\.js$/,
-        include: [
-          `${__dirname}/src`,
-          `${__dirname}/test`
-        ],
-        exclude: /node_modules/,
-        loader: 'babel'
-      }] 
-    },
-    resolve: {
-      root: [
+    main: './src/main.js',
+  },
+  output: {
+    path: __dirname,
+    filename: '[name].js',
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      include: [
         `${__dirname}/src`,
+        `${__dirname}/test`,
       ],
-    },
-    devtool: 'inline-source-map'
-  }
-}
+      exclude: /node_modules/,
+      loader: 'babel',
+    }],
+  },
+  resolve: {
+    root: [
+      `${__dirname}/src`,
+    ],
+  },
+  devtool: 'inline-source-map',
+};
